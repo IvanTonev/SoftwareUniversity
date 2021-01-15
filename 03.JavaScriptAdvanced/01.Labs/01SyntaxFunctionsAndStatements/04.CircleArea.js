77e1d9aedebeae878ed7c3d526d31bf84a72fbf8
IvanTonev <76847232+IvanTonev@users.noreply.github.com>
2021-01-15 23:02:28 +0200

    Add

---
+++ 03.JavaScriptAdvanced/01.Labs/01SyntaxFunctionsAndStatements/04.CircleArea.js
@@ -0,0 +1,10 @@
+function calculateCircleArea(input) {
+    let result;
+    let inputType = typeof (input);
+    if (inputType === 'number') {
+        result = (Math.pow(input, 2) * Math.PI).toFixed(2);
+    } else {
+        result = `We can not calculate the circle area, because we receive a ${inputType}.`;
+    }
+    console.log(result);
+}
