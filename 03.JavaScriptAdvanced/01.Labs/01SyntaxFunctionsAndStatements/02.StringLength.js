59035832a0711d39833b49bd913fb69de80ac7c8
IvanTonev <76847232+IvanTonev@users.noreply.github.com>
2021-01-15 23:01:26 +0200

    Add

---
+++ 03.JavaScriptAdvanced/01.Labs/01SyntaxFunctionsAndStatements/02.StringLength.js
@@ -0,0 +1,6 @@
+function stringLength(firstString, secondString, thirdString) {
+    let sum = firstString.length + secondString.length + thirdString.length;
+    let averageLength = sum / 3;
+    console.log(sum);
+    console.log(Math.floor(averageLength));
+}
