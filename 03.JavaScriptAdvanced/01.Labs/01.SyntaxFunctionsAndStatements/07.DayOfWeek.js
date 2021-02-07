ec023916be2812a393f6c2996f03e1cc0484484f
IvanTonev <76847232+IvanTonev@users.noreply.github.com>
2021-02-07 15:39:57 +0200

    Add

---
+++ 03.JavaScriptAdvanced/01.Labs/01.SyntaxFunctionsAndStatements/07.DayOfWeek.js
@@ -0,0 +1,30 @@
+function getDayOfWeek(weekday) {
+    let result;
+    switch (weekday) {
+        case 'Monday':
+            result = 1;
+            break;
+        case 'Tuesday':
+            result = 2;
+            break;
+        case 'Wednesday':
+            result = 3;
+            break;
+        case 'Thursday':
+            result = 4;
+            break;
+        case 'Friday':
+            result = 5;
+            break;
+        case 'Saturday':
+            result = 6;
+            break;
+        case 'Sunday':
+            result = 7;
+            break;
+        default:
+            result = 'error';
+            break;
+    }
+    return result;
+}
