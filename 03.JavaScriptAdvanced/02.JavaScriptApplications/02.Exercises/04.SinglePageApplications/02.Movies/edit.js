23c6cb7426aa5658886930a210e7da6d044da540
IvanTonev <76847232+IvanTonev@users.noreply.github.com>
2021-03-12 22:39:20 +0200

    Add

---
+++ 03.JavaScriptAdvanced/02.JavaScriptApplications/02.Exercises/04.SinglePageApplications/02.Movies/edit.js
@@ -0,0 +1,13 @@
+let main;
+let section;
+
+
+export function setupEdit(mainTarget, sectionTarget) {
+    main = mainTarget;
+    section = sectionTarget;
+}
+
+export async function showEdit() {
+    main.innerHTML = '';
+    main.appendChild(section);
+}
