d9cc9be473e1f32130f2cdca69562f9dc37344a6
IvanTonev <76847232+IvanTonev@users.noreply.github.com>
2021-03-13 15:04:44 +0200

    Update app.js

--- 03.JavaScriptAdvanced/02.JavaScriptApplications/02.Exercises/04.SinglePageApplications/02.Movies/app.js
+++ 03.JavaScriptAdvanced/02.JavaScriptApplications/02.Exercises/04.SinglePageApplications/02.Movies/app.js
@@ -58,6 +58,29 @@ function setupNavigation() {
         event.preventDefault();
         showCreate();
     });
-
+    document.getElementById('logoutBtn').addEventListener('click', logout);
 }
 
+async function logout() {
+    const token = sessionStorage.getItem('authToken');
+    const response = await fetch('http://localhost:3030/users/logout', {
+        method: 'get',
+        headers: {
+            'X-Authorization': token
+        }
+    });
+
+    if (response.ok) {
+        sessionStorage.removeItem('authToken');
+        sessionStorage.removeItem('userId');
+        sessionStorage.removeItem('email');
+
+        [...document.querySelectorAll('nav .user')].forEach(l => l.style.display = 'none');
+        [...document.querySelectorAll('nav .guest')].forEach(l => l.style.display = 'block');
+
+        showHome();
+    } else {
+        const error = await response.json();
+        alert(error.message);
+    }
+}
