e9745a9eca707a954dc65b198f7178fc6728f1f2
IvanTonev <76847232+IvanTonev@users.noreply.github.com>
2021-03-13 15:05:11 +0200

    Update create.js

--- 03.JavaScriptAdvanced/02.JavaScriptApplications/02.Exercises/04.SinglePageApplications/02.Movies/create.js
+++ 03.JavaScriptAdvanced/02.JavaScriptApplications/02.Exercises/04.SinglePageApplications/02.Movies/create.js
@@ -1,3 +1,36 @@
+import {showDetails} from './details.js'
+
+async function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const movie = {
+        title: formData.get('title'),
+        description: formData.get('description'),
+        img: formData.get('imageUrl')
+    };
+    if (movie.title == '' || movie.description == '' || movie.img == '') {
+        return alert('All fields are required');
+    }
+
+    const respone = await fetch('http://localhost:3030/data/movies', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': sessionStorage.getItem('authToken')
+        },
+        body : JSON.stringify(movie)
+    });
+
+    if (respone.ok) {
+        const moive = await respone.json();
+        showDetails(movie._id);
+
+    } else {
+        const error = await response.json();
+        alert(error.message);
+    }
+}
+
 let main;
 let section;
 
@@ -5,6 +38,10 @@ let section;
 export function setupCreate(mainTarget, sectionTarget) {
     main = mainTarget;
     section = sectionTarget;
+
+    const form = section.querySelector('form');
+    form.addEventListener('submit', onSubmit);
+
 }
 
 export async function showCreate() {
