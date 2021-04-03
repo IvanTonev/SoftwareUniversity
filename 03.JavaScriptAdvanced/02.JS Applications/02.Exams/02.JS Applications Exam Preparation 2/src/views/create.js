e6c3247684c4d8417836a81d353ae33f34e5326a
IvanTonev <76847232+IvanTonev@users.noreply.github.com>
2021-04-03 23:36:39 +0300

    Create create.js

---
+++ 03.JavaScriptAdvanced/02.JS Applications/02.Exams/02.JS Applications Exam Preparation 2/src/views/create.js
@@ -0,0 +1,64 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+import { createCarListing } from '../api/data.js';
+
+const createTemplate = (onSubmit) => html`
+<section id="create-listing">
+    <div class="container">
+        <form @submit=${onSubmit} id="create-form">
+            <h1>Create Car Listing</h1>
+            <p>Please fill in this form to create an listing.</p>
+            <hr>
+
+            <p>Car Brand</p>
+            <input type="text" placeholder="Enter Car Brand" name="brand">
+
+            <p>Car Model</p>
+            <input type="text" placeholder="Enter Car Model" name="model">
+
+            <p>Description</p>
+            <input type="text" placeholder="Enter Description" name="description">
+
+            <p>Car Year</p>
+            <input type="number" placeholder="Enter Car Year" name="year">
+
+            <p>Car Image</p>
+            <input type="text" placeholder="Enter Car Image" name="imageUrl">
+
+            <p>Car Price</p>
+            <input type="number" placeholder="Enter Car Price" name="price">
+
+            <hr>
+            <input type="submit" class="registerbtn" value="Create Listing">
+        </form>
+    </div>
+</section>`;
+
+export async function createPage(ctx) {
+    ctx.render(createTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+
+        const brand = formData.get('brand').trim();
+        const model = formData.get('model').trim();
+        const description = formData.get('description').trim();
+        const year = formData.get('year').trim();
+        const imageUrl = formData.get('imageUrl').trim();
+        const price = formData.get('price').trim();
+
+        const listing = {
+            brand: brand,
+            model: model,
+            description: description,
+            year: year,
+            imageUrl: imageUrl,
+            price:price
+        };
+
+        await createCarListing(listing)
+        ctx.page.redirect('/all-listings')
+    }
+}
